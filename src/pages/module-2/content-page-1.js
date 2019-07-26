faf4bd8b35af6fadbc1d6f4948c83ce744f973a1
klpoland1 <45948479+klpoland1@users.noreply.github.com>
2019-07-26 10:33:11 -0400

    Update and rename content-page-2.js to content-page-1.js

---
+++ src/pages/module-2/content-page-1.js
@@ -0,0 +1,3 @@
+import React from "react"
+
+export default () => <div>This is another page representing /module-2/content-page-1</div>
--- src/pages/module-2/content-page-2.js
+++
@@ -1,3 +0,0 @@
-import React from "react"
-
-export default () => <div>This is another page representing /module-1/content-page-2</div>
